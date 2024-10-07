ea45f49d993de03cccbbeefb15f08ccab1747dcc
irsyadhab <119821636+irsyadhab@users.noreply.github.com>
2024-10-07 12:48:41 +0700

    Create InputReader.java

---
+++ InputReader.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class InputReader {
+
+    private Scanner scanner;
+
+    public InputReader() {
+        scanner = new Scanner(System.in);
+    }
+
+    public String getInput() {
+        System.out.print("> ");  // Prompt
+        return scanner.nextLine().trim();
+    }
+}
